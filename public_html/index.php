a19fc19a2a6e047946d94bc2906b47a14e36245c
Eddie Ferrer <eddie@parthenonsoftware.com>
2016-05-02 11:15:40 -0700

    initial commit of new front end stack and the new homepage

--- public_html/index.php
+++ public_html/index.php
@@ -2,105 +2,65 @@
 require_once($_SERVER['DOCUMENT_ROOT']. "/resources/php/infocusscripts.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/resources/php/header.php");
 echo PHP_EOL . '<link rel="canonical" href="http://' .  $_SERVER['SERVER_NAME'] . '"/>' . PHP_EOL;
-
 ?>
+
 <title>InFocus | Collaboration That Works</title>
 <meta name="description" content="Discover advanced projection equipment offered by InFocus. Global leader in LCD projectors and DLP projection, Mondopad products and projector accessories." />
 <meta name="keywords" content="infocus,mondopad,projector,projection,DLP,LCD,LCD display,digital display,visual communications,projector accessories,infocus company,digital technology,projection equipment,monopad, communication, touchscreen" />
-<script type="text/Javascript" src="/resources/js/idangerous.swiper-2.4.min.js"></script>
-<link rel="stylesheet" href="/resources/css/idangerous.swiper.css" />
-<base target="_parent" />
-<script>
-var pagetype = "index";
-</script>
-<style>
-.swiper-slide img{
-width:100%;
-}
-#ytplayer {
-position:absolute;
-z-index:100;
-}
-
-.device{
-z-index:10;
-}
-
-.CTA li:nth-child(3), .CTA li:nth-child(4){
-	padding-top:1%;
-}
-
-.tout44 {
-position: absolute;
-bottom: 10%;
-width: 12%;
-left: 4%;
-background: #404042;
-padding: 1% 10%;
-color:#1BA1E2;-webkit-box-shadow: 4px 7px 5px -3px rgba(0,0,0,0.75);
--moz-box-shadow: 4px 7px 5px -3px rgba(0,0,0,0.75);
-box-shadow: 4px 7px 5px -3px rgba(0,0,0,0.75);
-border-radius: 5px;
-display:none;
-}
-
-.tout44:hover {
-background: #F2F2F2;
-}
-
-.tout22 {
-padding: 1% 6%;
-position: absolute;
-bottom: 10%;
-width: 25%;
-left: 57%;
-background: #404042;
-color: #E81469;
-text-align: center;
--webkit-box-shadow: 4px 7px 5px -3px rgba(0,0,0,0.75);
--moz-box-shadow: 4px 7px 5px -3px rgba(0,0,0,0.75);
-box-shadow: 4px 7px 5px -3px rgba(0,0,0,0.75);
-border-radius: 5px;
-display:none;
-}
-
-.tout22:hover{
-background: #F2F2F2;
-}
-
-.large-CTA img{
-	max-width: 95.5%;
-}
-.crop {
-    width: 100%;
-    overflow: hidden;
-}
-
-.crop img {
-    width: 100%;
-    margin: 0 0 0 0%;
-}
-.arrow-left:hover{
-opacity:0.5;
-}
-
-.arrow-right:hover{
-opacity:0.5;
-}
-
-.linkspan{
-z-index:7;
-}
-
-</style>
 <?php
 if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $homedir.'/resources/overviews/', 'admin,salespublisher,saleseditor,marketingpublisher,marketingeditor,editor', ".src", "overview", "true", "SavePage","overview");}
 
 ?>
+</head>
+<body class="" style="">
+	<div class="top-bar">
+		<div class="row">
+			<div class="top-bar">
+				<?php include($homedir . "/resources/html/mainmenu.html"); ?>
+			</div>
+		</div>
+	</div>
+	<!-- TODO remove these br's when header gets coded -->
+	<br><br><br><br>
+	<br><br><br><br>
 
-	</head>
-	<body class="" style="">
-		<?php include($homedir . "/resources/html/mainmenu.html"); ?>
+	<div class="row left_bordered">
+		<div class="small-12 medium-5 columns">
+			<h2 class="lead_text headline">Focus on what matters</h2>
+			<p class="lead_text paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </h1>
+
+		</div>
+		<div class="small-12 medium-7 columns">
+			<img src="http://placehold.it/804x699">
+		</div>
+	</div>
+		<div class="small-12 medium-6 large-5 columns">
+			<h3>My Awesome Product</h3>
+			<p>Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus tortor. Nulla facilisi. Duis aliquet egestas purus in.</p>
+			<label>Size
+				<select>
+					<option value="husker">Small</option>
+					<option value="starbuck">Medium</option>
+					<option value="hotdog">Large</option>
+					<option value="apollo">Yeti</option>
+				</select>
+			</label>
+			<div class="row">
+				<div class="small-3 columns">
+					<label for="middle-label" class="middle">Quantity</label>
+				</div>
+				<div class="small-9 columns">
+					<input type="text" id="middle-label" placeholder="One fish two fish">
+				</div>
+			</div>
+			<a href="#" class="button large expanded">Buy Now</a>
+			<div class="small secondary expanded button-group">
+				<a class="button">Facebook</a>
+				<a class="button">Twitter</a>
+				<a class="button">Yo</a>
+			</div>
+		</div>
+	</div>
 <div class="content" style="width:100%;padding-top: 87px;">
 <div id="ytplayer" class="player"></div>
 
@@ -110,11 +70,11 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 <div class="swiper-container" >
 <div id="swiperwrap" class="swiper-wrapper"><!-- SwipeGenerated -->
 <div class="swiper-slide sedit" style="position:relative"> <div aria-describedby="" title="Rich Text Editor, editor1" aria-label="Rich Text Editor, editor1" role="textbox" style="position: relative;" spellcheck="false" tabindex="0" class="crop cmsedit "><img alt="InFocus Mondopad Collaboration System" src="/resources/images/InFocus-Mondopad-Renovate-Conference-Room"></div><a style="height: 25px;" href="/displays/MONDOPAD-SERIES"><span class="linkspan" style="position: absolute; top: 82.1053%; width: 10.8424%; height: 8.94737%; left: 6.83903%; opacity: 0;"></span></a><a style="height: 25px;" href="/displays/MONDOPAD-SERIES"><span class="linkspan" style="position: absolute; top: 8.1579%; width: 36.7807%; height: 28.6842%; left: 6.25521%; opacity: 0;"></span></a></div><div class="swiper-slide sedit" style="position:relative"> <div aria-describedby="" title="Rich Text Editor, editor7" aria-label="Rich Text Editor, editor7" role="textbox" style="position: relative;" spellcheck="false" tabindex="0" class="crop cmsedit"><img alt="Free whiteboard and video calling software with BigTouch purchse" src="/resources/images/InFocus-BigTouch-Free-Software-Promo-Home"></div><a style="height: 25px;" href="/bigtouch-free-software"><span class="linkspan" style="position: absolute; top: 79.5812%; width: 10.7705%; height: 8.63874%; left: 55.4267%; opacity: 0;"></span></a><a style="height: 25px;" href="/bigtouch-free-software"><span class="linkspan" style="position: absolute; top: -0.52356%; width: 49.2958%; height: 97.9058%; left: 3.06545%; opacity: 0;"></span></a><a style="height: 25px;" href="/displays/BIGTOUCH-SERIES"><span class="linkspan" style="position: absolute; top: 19.1099%; width: 36.7854%; height: 14.9215%; left: 55.3438%; opacity: 0;"></span></a></div><div class="swiper-slide sedit" style="position:relative"> <div aria-describedby="" title="Rich Text Editor, editor3" aria-label="Rich Text Editor, editor3" role="textbox" spellcheck="false" tabindex="0" style="position: relative;" class="crop cmsedit"><img alt="InFocus Low Cost 1080p HD Projector" src="/resources/images/InFocus-1080p-HD-Projectors-Home"></div><a style="height: 28px;" href="https://www.infocusdirect.com/projectors/high-definition"><span class="linkspan" style="position: absolute; top: 83.2957%; width: 10.8011%; height: 9.25508%; left: 75.8941%; opacity: 0;"></span></a><a style="height: 25px;" href="/hd-projector-lineup"><span class="linkspan" style="position: absolute; top: 83.07%; width: 10.9442%; height: 9.25508%; left: 58.7983%; opacity: 0;"></span></a><a style="height: 25px;" href="/hd-projector-lineup"><span class="linkspan" style="position: absolute; top: 7.81671%; width: 18.4143%; height: 12.6685%; left: 63.6829%; opacity: 0;"></span></a><a style="height: 25px;" href="/hd-projector-lineup"><span class="linkspan" style="position: absolute; top: 32.6146%; width: 24.0409%; height: 38.814%; left: 61.0401%; opacity: 0;"></span></a><a style="height: 25px;" href="/hd-projector-lineup"><span class="linkspan" style="position: absolute; top: 5.92992%; width: 43.4783%; height: 86.7924%; left: 9.37766%; opacity: 0;"></span></a></div>
- 
- 
- 
- 
- 
+
+
+
+
+
 
 
 
@@ -146,8 +106,8 @@ $(document).ready(function() {
 mySwiper = new Swiper('.swiper-container',{
     pagination: '.pagination',
     loop:true,
-	speed:750, 
-	autoplay: 5000,  
+	speed:750,
+	autoplay: 5000,
 	calculateHeight: true,
 	initialSlide:0,
 	grabCursor:true,
@@ -162,10 +122,10 @@ mySwiper = new Swiper('.swiper-container',{
 	},
 	onSlideClick: function(){
 	  if(mySwiper.activeIndex==2){
-	  
+
 
 	}
-	
+
 	// if(mySwiper.activeIndex == 0 && (mySwiper.touches['current']-mySwiper.touches['start']) > 200){
 	// mySwiper.swipeTo(mySwiper.slides.length-1,1000,false);
 	// }
@@ -199,7 +159,7 @@ mySwiper = new Swiper('.swiper-container',{
     mySwiper.swipeNext()
 	// }
   })
-  
+
     $('#ytcover').on('mousedown', function(e) {
     $(this).data('p0', { x: e.pageX, y: e.pageY });
 }).on('mouseup', function(e) {
@@ -210,7 +170,7 @@ if(e.which != 1){return;}
     if (d < 4) {
 		if(typeof player === 'undefined'){
 			loadPlayer();
-			
+
 			  }
 		else{
 		playVideo();
@@ -219,19 +179,19 @@ if(e.which != 1){return;}
 	else{
 	player.pauseVideo();
 	}
-	
+
 	$('.pagination').on('click',function(e){player.pauseVideo();});
 
 })
 
- 
+
 $(window).resize(function(){
   mySwiper.reInit();
 })
   var hash = window.location.hash;
   hash = hash.substring(1);
   if(hash.length>0){
-    
+
 $.get(
    "/resources/forms/" + hash,
    function(data, textStatus, jqXHR) {
@@ -271,9 +231,12 @@ $.get(
 	<li class="image-set"><a href="/projectors/office-classroom/IN3130a-Series"><img alt="InFocus IN3130a Network Projector Series" src="/resources/images/InFocus-IN3130a-Network-Projector.png"></a></li>
 
 </div>
-<!-- EndCTAGenerated --></div> 
+<!-- EndCTAGenerated --></div>
 </div>
- 
+
+<script>
+    $(document).foundation();
+</script>
 
 <script type="text/javascript">
   // Load the IFrame Player API code asynchronously.
@@ -305,14 +268,14 @@ $.get(
 			$('#ytplayer').animate({'opacity':'0'},0).animate({'opacity':'1'},500);
 			 mySwiper.stopAutoplay()
 
-			 
+
  }
-  
+
       // 4. The API will call this function when the video player is ready.
       function onPlayerReady(event) {
         event.target.playVideo();
 
-		
+
 
       }
 
@@ -337,7 +300,7 @@ function onytplayerStateChange(newState) {
         player.stopVideo();
       }
        function playVideo() {
-	   
+
 	   if(player.getPlayerState() == 1){
 	    player.pauseVideo();
 	   }
@@ -346,13 +309,13 @@ function onytplayerStateChange(newState) {
 
 			 player.playVideo();
 		}
-		
+
       }
 
 function coverClick(){
 		if(typeof player === 'undefined'){
 		loadPlayer();
-		
+
 		  }
 		else{
 		playVideo();
@@ -362,9 +325,6 @@ function coverClick(){
 				<footer id="site-info" >
 				<?php include($homedir . "/resources/html/footer.html"); ?>
 				</footer>
-<script>
-
-</script>
 
 
 </body></html>
--- public_html/resources/php/header.php
+++ public_html/resources/php/header.php
@@ -1,28 +1,48 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?= $lang; ?>" dir="ltr" class="wf-pragmaticawebcondensed-n3-active wf-pragmaticawebcondensed-n4-active wf-pragmaticawebcondensed-n9-active wf-active"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta charset="UTF-8">
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="icon" href="/favicon.png" type="image/gif" sizes="32x32">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+
+
+<!-- obsolete script
 <script type="text/Javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script type="text/Javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+<script type="text/Javascript" src="/resources/js/jquery.flexnav.min.js"></script>
+<script>
+window.onbeforeunload = function () {
+try{$(".flexnav").flexNav();
+ }catch(e){}}
+</script>
+
+
+end obsolete scripts -->
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="/resources/js/vendor/foundation.min.js"></script>
+
+<!-- these can be removed after -->
+
+
 <script type="text/javascript" src="//use.typekit.net/lbn0ick.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 <script type="text/Javascript" src="/resources/js/tagSort.js"></script>
 <script type="text/Javascript" src="/resources/js/InFocusCollection.js"></script>
 <script type="text/Javascript" src="/resources/js/jquery.colorbox.js"></script>
 <link rel="stylesheet" href="/resources/css/colorbox.css" />
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+
 <link rel="stylesheet" href="/resources/css/core.css" />
 <?php if($lang != "en"){echo '<link rel="stylesheet" href="/resources/css/Non-en.css" />'; }?>
 <link rel="stylesheet" href="/resources/css/flexnav.css" />
 <script type="text/Javascript" src="/resources/js/jquery.hoverIntent.min.js"></script>
-<script type="text/Javascript" src="/resources/js/jquery.flexnav.min.js"></script>
 <script src="/resources/js/jquery.fitvids.js"></script>
 <!--[if IE]>
-    <link href="/resources/css/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
-  <![endif]-->
-  
+  <link href="/resources/css/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<![endif]-->
+<link rel="stylesheet" href="/resources/css/vendor/foundation.min.css" />
+<link rel="stylesheet" href="/resources/css/infocus.min.css" />
 <!-- Start Visual Website Optimizer Asynchronous Code -->
 <script type='text/javascript'>
 var _vwo_code=(function(){
@@ -34,8 +54,3 @@ use_existing_jquery=false,
 f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
 </script>
 <!-- End Visual Website Optimizer Asynchronous Code -->
-  <script>
-window.onbeforeunload = function () {
-try{$(".flexnav").flexNav();
- }catch(e){}} 
-  </script>
