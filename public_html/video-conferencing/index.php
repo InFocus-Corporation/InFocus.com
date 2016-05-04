589bd5ea4bc28d036c142f2832cdb753394414cf
Eddie Ferrer <eddie@parthenonsoftware.com>
2016-05-04 15:20:41 -0700

    added images and links refs #26595

--- public_html/video-conferencing/index.php
+++ public_html/video-conferencing/index.php
@@ -18,7 +18,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 </head>
 <body class="" style="">
 	<?php include($homedir . "/resources/html/mainmenu.html"); ?>
-	<div class="hero_row">
+	<div class="hero_row transparent-border-right-40-orange hero-row--video_conferencing">
 		<div class="row">
 			<div class="small-11 medium-7 large-5 columns lead_text">
 				<h2 class="lead_text--secondary_headline">
@@ -35,14 +35,62 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 
 	</div>
 	<div class="row">
-		<div class="small-12 medium-7 columns">
+		<div class="small-12 medium-8 columns color_stripe_column">
 			<div class="white color_stripe"></div>
-			<img src="https://placehold.it/750x538" alt="">
+			<img src="/resources/static/images/video-conferencing/video-conferencing-mondopad--InFocus-Lifestyle-Mondopad-DualScreen-LightOffice-300dpi-CMYK.jpg" alt="">
 		</div>
-		<div class="small-12 medium-5 columns">
-			<h2 class="lead_text--secondary_headline">Mondopad</h2>
+		<div class="small-12 medium-4 columns">
+			<h3 class="lead_text--secondary_headline">Mondopad</h3>
 			<p class="lead_text--paragraph">Instantlybring people together from anywhere in the world to visually collaborate on the same content and do better work in less time.</p>
 			<a href="/displays/MONDOPAD-SERIES" class="button button--primary">learn more</a>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 medium-8 columns color_stripe_column">
+			<div class="white color_stripe"></div>
+			<img src="/resources/static/images/video-conferencing/video-conference-conx-wall-conx-exec--ConXWall-Hero-Image-3x4-1B-96dpi-RGB.jpg" alt="">
+		</div>
+		<div class="small-12 medium-4 columns">
+			<h3 class="lead_text--secondary_headline">ConX Wall and ConX Exec</h3>
+			<p class="lead_text--paragraph">Instantlybring people together from anywhere in the world to visually collaborate on the same content and do better work in less time.</p>
+			<a href="/conx-wall-exec" class="button button--primary">learn more</a>
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="small-12 medium-8 columns color_stripe_column">
+			<div class="white color_stripe"></div>
+			<img src="/resources/static/images/video-conferencing/video-conferencing-conx-phone--ConXWall-Hero-Image-3x4-1B-96dpi-RGB.jpg" alt="">
+		</div>
+		<div class="small-12 medium-4 columns">
+			<h3 class="lead_text--secondary_headline">ConX Phone</h3>
+			<p class="lead_text--paragraph">Instantlybring people together from anywhere in the world to visually collaborate on the same content and do better work in less time.</p>
+			<a href="/peripherals/MVP100" class="button button--primary">learn more</a>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 columns text-center">
+			<h3 class="lead_text--secondary_headline">Video Conferencing Services</h3>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 medium-8 columns color_stripe_column">
+			<div class="white color_stripe"></div>
+			<img src="/resources/static/images/video-conferencing/video-conferencing-video-conferencing--InFocus-Lifestyle-MVP100-Construction-96dpi-RGB.jpg" alt="">
+		</div>
+		<div class="small-12 medium-4 columns">
+			<h3 class="lead_text--secondary_headline">ConX Video Meeting</h3>
+			<p class="lead_text--paragraph">Instantlybring people together from anywhere in the world to visually collaborate on the same content and do better work in less time.</p>
+			<a href="/peripherals/ConX-Series" class="paragraph-link">learn more</a>
+		</div>
+		<div class="small-12 medium-4 columns">
+			<h3 class="lead_text--secondary_headline">121 Video Calling</h3>
+			<p class="lead_text--paragraph">Instantlybring people together from anywhere in the world to visually collaborate on the same content and do better work in less time.</p>
+			<a href="/peripherals/121-Series" class="paragraph-link">learn more</a>
 		</div>
 	</div>
 
