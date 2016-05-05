b6045221f757a6dbf345b4b4ba798534c1cd6eb9
Kellen Biondi <kbiondi@parthenonsoftware.com>
2016-05-05 14:57:22 -0700

    fixes links and adds hero image
    refs #26597

--- public_html/displays/index.php
+++ public_html/displays/index.php
@@ -18,7 +18,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 </head>
 <body class="" style="">
 	<?php include($homedir . "/resources/html/mainmenu.html"); ?>
-	<div class="hero_row">
+	<div class="hero_row transparent-border-right-40-orange hero-row--touchscreens">
 		<div class="row">
 			<div class="small-11 medium-7 large-5 columns lead_text">
 				<h2 class="lead_text--secondary_headline">
@@ -37,7 +37,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 	<div class="row">
 		<div class="small-12 medium-7 columns">
 			<div class="white color_stripe"></div>
-			<img src="../resources/static/images/InFocus-Lifestyle-Mondopad-DualScreen-LightOffice-300dpi-CMYK.jpg" alt="">
+			<img src="../resources/static/images/touchscreens/InFocus-Lifestyle-Mondopad-DualScreen-LightOffice-300dpi-CMYK.jpg" alt="">
 		</div>
 		<div class="small-12 medium-5 columns">
 			<h2 class="lead_text--secondary_headline">Mondopad</h2>
@@ -48,45 +48,45 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 	<div class="row">
 		<div class="small-12 medium-7 columns">
 			<div class="white color_stripe"></div>
-			<img src="../resources/static/images/InFocus-Lifestyle-JTouch-K12-MuscleDiagram-300dpi-CMYK.jpg" alt="">
+			<img src="../resources/static/images/touchscreens/InFocus-Lifestyle-JTouch-K12-MuscleDiagram-300dpi-CMYK.jpg" alt="">
 		</div>
 		<div class="small-12 medium-5 columns">
 			<h2 class="lead_text--secondary_headline">JTouch</h2>
 			<p class="lead_text--paragraph">Connect the InFocus JTouch touchscreen display to your notebook to create a bright, colorful touch experience that engages audiences in classrooms and beyond.</p>
-			<a href="/displays/MONDOPAD-SERIES" class="button button--primary">learn more</a>
+			<a href="/displays/JTOUCH-Series" class="button button--primary">learn more</a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="small-12 medium-7 columns">
 			<div class="white color_stripe"></div>
-			<img src="../resources/static/images/InFocus-DigiEasel-Business-Presentation.jpg" alt="">
+			<img src="../resources/static/images/touchscreens/InFocus-DigiEasel-Business-Presentation.jpg" alt="">
 		</div>
 		<div class="small-12 medium-5 columns">
 			<h2 class="lead_text--secondary_headline">DigiEasel</h2>
 			<p class="lead_text--paragraph">Draw, write, and capture notes or share your device’s screen on a professional, high-tech interactive touch display that's affordable, fun to use and impressive to visitors.</p>
-			<a href="/displays/MONDOPAD-SERIES" class="button button--primary">learn more</a>
+			<a href="/displays/DigiEasel-Series" class="button button--primary">learn more</a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="small-12 medium-7 columns">
 			<div class="white color_stripe"></div>
-			<img src="../resources/static/images/InFocus-Lifestyle-QTablet-Construction-300dpi-CMYK.jpg" alt="">
+			<img src="../resources/static/images/touchscreens/InFocus-Lifestyle-QTablet-Construction-300dpi-CMYK.jpg" alt="">
 		</div>
 		<div class="small-12 medium-5 columns">
 			<h2 class="lead_text--secondary_headline">QTablet</h2>
 			<p class="lead_text--paragraph">InFocus Q Tablets have the power and features to be productive at work or school and entertained at home – all at great low prices.</p>
-			<a href="/displays/MONDOPAD-SERIES" class="button button--primary">learn more</a>
+			<a href="/displays/Q-Tablet-Series" class="button button--primary">learn more</a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="small-12 medium-7 columns">
 			<div class="white color_stripe"></div>
-			<img src="../resources/static/images/InFocus-INF5711-Win8-Front.jpg" alt="">
+			<img src="../resources/static/images/touchscreens/InFocus-INF5711-Win8-Front.jpg" alt="">
 		</div>
 		<div class="small-12 medium-5 columns">
 			<h2 class="lead_text--secondary_headline">BigTouch</h2>
 			<p class="lead_text--paragraph">Windows 8 combines the fun of a tablet with the productivity of a PC, and the InFocus BigTouch amplifies that beautiful, fast and fluid touch experience for your shared work space.</p>
-			<a href="/displays/MONDOPAD-SERIES" class="button button--primary">learn more</a>
+			<a href="/displays/BIGTOUCH-Series" class="button button--primary">learn more</a>
 		</div>
 	</div>
 
