322e865758143de0cd60f54c2470ca115c90e255
Eddie Ferrer <eddie@parthenonsoftware.com>
2016-05-06 08:06:00 -0700

    styled new 404 page refs #26693

--- public_html/404.php
+++ public_html/404.php
@@ -2,28 +2,30 @@
 header("HTTP/1.0 404 Not Found");
 //require_once($_SERVER['DOCUMENT_ROOT']. "/resources/php/infocusscripts.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/resources/php/header.php");
-    if( substr($_SERVER['DOCUMENT_ROOT'],-11) == "public_html"){$lang = "en";}
-    else{$lang = substr($_SERVER['DOCUMENT_ROOT'],-2);}
+if( substr($_SERVER['DOCUMENT_ROOT'],-11) == "public_html"){$lang = "en";}
+else{$lang = substr($_SERVER['DOCUMENT_ROOT'],-2);}
 ?>
-<link rel="stylesheet" href="/resources/css/404.css" />
-	</head>
-	<body>
-		<?php include($_SERVER['DOCUMENT_ROOT'] . "/resources/html/mainmenu.html"); ?>
+<link rel="stylesheet" href="/resources/css/vendor/foundation.min.css" />
+<link rel="stylesheet" href="/resources/css/infocus.min.css" />
+<link rel="stylesheet" href="/resources/css/page-404.min.css" />
 
-<link href='http://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'>
-<section class="fade">
-<div class="content">
-<div class="C9">
-<div class="error" style="height:300px;padding-top:150px;">
-  <p id="error">E<span>r</span>ror</p><br><br>
-  <p id="code">4<span>0</span><span>4</span></p>
-</div>
-</div>
-		</div>
-			</section>
+</head>
+<body class="page--404">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/resources/html/mainmenu.html"); ?>
+  <div class="hero_row hero-row--404 row--full-height">
+    <div class="row">
+      <div class="small-10 small-offset-1 medium-8 medium-offset-2 columns">
+        <h2 class="text-center text--orange">404 Error</h2>
+        <h3 class="text-center text--white">&#8212;</h3>
+        <h2 class="lead_text--secondary_headline text-center text--white">We regret this page is missing or unavailable.</h2>
+        <p class="text-center text--white">You might try searching for a keyword(s) in the navigation above.</p>
+      </div>
+    </div>
+  </div>
 
-				<footer id="site-info" >
-				<?php include($_SERVER['DOCUMENT_ROOT'] ."/resources/html/footer.html"); ?>
-				</footer>
+
+  <footer id="site-info" >
+    <?php include($_SERVER['DOCUMENT_ROOT'] ."/resources/html/footer.html"); ?>
+  </footer>
 </body>
 </html>
