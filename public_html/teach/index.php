3e0a12ce6a2ac844ab6a7c118eed20045f78dead
Kellen Biondi <kbiondi@parthenonsoftware.com>
2016-05-10 10:48:40 -0700

    adds links to teach and train
    refs #26722

--- public_html/teach/index.php
+++ public_html/teach/index.php
@@ -129,21 +129,21 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
                 Engage your students and please the staff of your K-12 school with the most affordable 65" interactive whiteboard display on the market.
             </p>
             <br>
-            <a href="#" class="text_module_link">View $1,999 (US) Whiteboard</a>
+            <a href="/displays/INF6501w" class="text_module_link">View $1,999 (US) Whiteboard</a>
         </div>
         <div class="small-12 large-4 columns text-center">
             <p class="lead_text--paragraph">
                 Connect devices wirelessly to your display and create a collaborative environment with an interactive whiteboard that also has a built-in web browser.
             </p>
             <br>
-            <a href="#" class="text_module_link">View Wireless Whiteboards</a>
+            <a href="/displays/JTOUCH-LightCast-SERIES" class="text_module_link">View Wireless Whiteboards</a>
         </div>
         <div class="small-12 large-4 columns text-center">
             <p class="lead_text--paragraph">
                 Roll your whiteboard where it's needed most, within the room or between rooms, when you mount the display on a mobile cart.
             </p>
             <br>
-            <a href="#" class="text_module_link">View Mobile Cart</a>
+            <a href="/accessories/mondopad-accessories" class="text_module_link">View Mobile Cart</a>
         </div>
     </div>
     <!-- END TEXT MODULE A -->
@@ -151,7 +151,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
     <!-- FEATURE MODULE JTOUCH -->
     <div class="video_module">
         <div class="row">
-            <a href="#">
+            <a href="/displays/JTOUCH-SERIES#videos">
                 <h2 class="lead_text--secondary_headline">See how JTouch invigorated<br>a 5th grade classroom</h2>
             </a>
         </div>
@@ -164,8 +164,8 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
             <h3 class="lead_text--secondary_headline">Distance<br>learning within<br>reach</h3>
             <p class="lead_text--paragraph">Students, teachers or guest speakers can join your class or training from remote locations with high-quality, reliable and affordable video conferencing solutions.</p>
             <div class="feature_links">
-                <a href="#" class="feature_link">View Mondopad Collaboration Displays</a><br>
-                <a href="#" class="feature_link">View CONX Video Meeting Rooms</a>
+                <a href="/displays/MONDOPAD-SERIES" class="feature_link">View Mondopad Collaboration Displays</a><br>
+                <a href="/peripherals/ConX-Series" class="feature_link">View CONX Video Meeting Rooms</a>
             </div>
         </div>
         <div class="small-12 medium-6 columns color_stripe_column">
@@ -187,7 +187,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
     			<p class="lead_text--paragraph">Your IT and finance departments will appreciate that our JTouch Whiteboards and displays work with the computers, tablets and software you already use.</p>
                 <br><br>
                 <div class="feature_links">
-                    <a href="#" class="feature_link">View Whiteboard Displays</a><br>
+                    <a href="/displays/JTOUCH-Whiteboard-SERIES" class="feature_link">View Whiteboard Displays</a><br>
                 </div>
 
             </div>
@@ -225,11 +225,11 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
                 </p>
             </div>
             <div class="module_links">
-                <a href="/teach/" class="promo_module_link first_link">
+                <a href="/projectors/short-throw" class="promo_module_link first_link">
                     View Short Throw <br>
                     Projectors
                 </a>
-                <a href="/collaborate/" class="promo_module_link">
+                <a href="/accessories/mounts/INA-SMRTADPT" class="promo_module_link">
                     View Smart&trade; Mount <br>
                     Adapter
                 </a>
@@ -251,8 +251,8 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
                 <li>Create a secure wireless network access point</li>
             </ul>
             <div class="feature_links">
-                <a href="#" class="feature_link">View Lightshow 4 Wireless Adapter</a><br>
-                <a href="#" class="feature_link">Read How A Teacher Uses Liteshow 4</a>
+                <a href="/peripherals/INLITESHOW4" class="feature_link">View Lightshow 4 Wireless Adapter</a><br>
+                <a href="http://www.thenerdyteacher.com/2015/03/infocus-liteshow4-makes-collaboration.html" target="blank" class="feature_link">Read How A Teacher Uses Liteshow 4</a>
             </div>
         </div>
         <div class="small-12 medium-6 columns color_stripe_column">
@@ -269,7 +269,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
             <p class="lead_text--paragraph">
                 Your school can get special pricing, extended warranties and complimentary training when you buy select touchscreens or projectors from and authorized Inspire Dealer.
             </p>
-            <a href="/about" class="text_module_link">
+            <a href="/inspire" class="text_module_link">
                 Learn how your school can save
             </a>
         </div>
