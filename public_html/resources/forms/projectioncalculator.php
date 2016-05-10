96662b1a72c3e12987b86b5cb6e31c7a4e7ac280
Chris Wilson <chris@parthenonsoftware.com>
2016-05-10 16:37:25 -0700

    Fixed Projector Calculator popup

--- public_html/resources/forms/projectioncalculator.php
+++ public_html/resources/forms/projectioncalculator.php
@@ -31,8 +31,20 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
- <script> //run all scripts and functions from here
- 
+<style>
+body {
+	background: #f2f2f0;
+}
+.navbar-default {
+    background: transparent;
+    border: none;
+}
+.navbar-header {
+	display: none;
+}
+</style>
+
+<script> //run all scripts and functions from here
  if(self==top){window.location = "/#projectioncalculator";}
 $(document).ready(function() {
     parent.$.colorbox.resize({
