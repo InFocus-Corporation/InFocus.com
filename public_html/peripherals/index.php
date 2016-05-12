24e5edf8aec5d3d984a67b6c8527bb8557839b18
Kellen Biondi <kbiondi@parthenonsoftware.com>
2016-05-11 17:42:32 -0700

    adds classes to peripherals hero for mobile
    refs #26760

--- public_html/peripherals/index.php
+++ public_html/peripherals/index.php
@@ -19,8 +19,8 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 <body class="peripherals_home" style="">
 	<?php include($homedir . "/resources/html/mainmenu.html"); ?>
 	<div class="hero_row transparent-border-right-40-orange hero-row--peripherals_home">
-		<div class="row">
-			<div class="small-11 medium-7 large-5 columns lead_text">
+		<div class="row hero_inner">
+			<div class="small-6 medium-5 columns lead_text">
 				<h2 class="lead_text--secondary_headline">
                     <img src="/resources/static/images/peripherals/peripherals_icon_temp.png" alt="" />
                     <br><br>
@@ -28,7 +28,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
 				</h2>
 				<p class="lead_text--paragraph">Ver o eos et accusamus et iusto odio dignissimos ducimus qui blanditis praesent voluptatum. Dolores et quas molestais excepturi sint occaecati cupiditate</p>
 			</div>
-			<div class="small-1 medium-3 color_stripe_column">
+			<div class="small-3 color_stripe_column">
 				<div class="dark_blue color_stripe"></div>
 			</div>
 		</div>
