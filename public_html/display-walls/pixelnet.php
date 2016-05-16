a77709f2deb26102bf5851943b0dde5bcb859927
Tim Tate <tim@parthenonsoftware.com>
2016-05-16 11:12:14 -0700

    started on pixelnet page

--- public_html/display-walls/pixelnet.php
+++ public_html/display-walls/pixelnet.php
@@ -14,9 +14,14 @@ echo PHP_EOL . '<link rel="canonical" href="http://' .  $_SERVER['SERVER_NAME'] 
 if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $homedir.'/resources/overviews/', 'admin,salespublisher,saleseditor,marketingpublisher,marketingeditor,editor', ".src", "overview", "true", "SavePage","overview");}
 ?>
 <style>
-div.info { float: left; }
-div.image-set { float: right; }
+div.image-set {
+    float: right;
+    width: 45%;
+}
 div.image-set img { width: 100%; }
+div.info {
+    width: 45%;
+}
 div.info .tagline {
     font-family: 'GoodPro';
     font-weight: bold;
@@ -39,6 +44,9 @@ div.info #blurb {
 #downloads .HeaderRow th:nth-child(3) { width: 160px; }
 #downloads table tbody tr td img {
     width: 45px;
+}
+#details_photos {
+    float: right;
 }
 </style>
 <script>
@@ -77,9 +85,12 @@ div.info #blurb {
             </div>
             <div class="C10 Col_child C6x4_child">
                 <div class="info">
-                    <strong class="tagline mysqledit" id="header"></strong>
-                    <span></span>
+                    <strong class="tagline mysqledit" id="header">A better way to get visual information where you need it.</strong>
+                    <span>PixelNet adopts Gigabit Ethernet technology to create a network of input and output nodes to drive high resolution, real time video walls.</span>
                     <div class="mysqledit" id="blurb">
+                        <p>A broad variety of PixelNet input nodes can be matched to almost any video source, while PixelNet output nodes connect to displays. Connected via CAT-6 cables, input and output nodes can be located as far as 100 meters from the switch. Using packet-switching technology, any information source can be shown on any display, such as a window on a single display, or as a window spanning multiple displays in a display wall.</p>
+
+                        <p>PixelNet makes creating complex topologies of inputs, outputs and switches simple, cost effective, and future proof. Massively scalable, the same component parts can scale from a single input distributed to a single output to hundreds of inputs and outputs. PixelNet input nodes are small, silent and use very little power. PixelNet Domain Control software provides an intuitive, object-oriented, drag-and-drop interface to control and manage multiple inputs, outputs and display walls.</p>
                     </div>
                 </div>
                 <div class="image-set">
@@ -101,7 +112,20 @@ div.info #blurb {
                                 <img src="/resources/static/images/display-walls/Product_PixelNet_AnalogHD.jpg" >
                             </div>
                             <div class="info cmsedit">
-                                overview
+                                <strong class="tagline mysqledit">
+                                    Flexible, Scable, Powerful.
+                                </strong>
+                                <div class="mysqledit">
+                                    <p>PixelNet is all about scalability. The same component parts can scale from a single input distributed to a single output to a configuration with hundreds of inputs and outputs. The networked system can support multiple display walls. Need to add another input? Add another PixelNet input node. Want to add more streaming IP sources? Add another StreamCenter. Expanding the display wall? Add PixelNet output nodes for the new displays.</p>
+                                </div>
+                                <hr>
+                                <strong class="tagline mysqledit">
+                                    A PixelNet Network
+                                </strong>
+                                <div class="mysqledit">
+                                    <img src="/resources/static/images/display-walls/PixelNet_System_rev.jpg">
+                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -109,11 +133,11 @@ div.info #blurb {
                 <div id="details">
                     <div class="C10 alternateDivChildL2">
                         <div>
-                            <div class="image-set cmsedit">
+                            <div class="image-set cmsedit" id="details_photos">
                                 <img src="/resources/static/images/display-walls/Product_PixelNet_AnalogHD.jpg" >
                             </div>
                             <div class="info cmsedit">
-                                details
+                                <p>PixelNet makes creating complex topologies of inputs, outputs and switches simple, cost effective, and future proof. Massively scalable, the same component parts can scale from a single input distributed to a single output to hundreds of inputs and outputs. PixelNet input nodes are small, silent and use very little power. PixelNet Domain Control software provides an intuitive, object-oriented, drag-and-drop interface to control and manage multiple inputs, outputs and display walls.</p>
                             </div>
                         </div>
                     </div>
