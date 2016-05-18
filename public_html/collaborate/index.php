5a09341124f8ec0637eefa968b957c3380e6cf4e
Kellen Biondi <kbiondi@parthenonsoftware.com>
2016-05-17 18:01:29 -0700

    adds line-height to video module collaborate-b to create room
    refs #26957

--- public_html/collaborate/index.php
+++ public_html/collaborate/index.php
@@ -120,7 +120,7 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
     </div>
     <!-- END MULTICOLUMN TEXT MODULE -->
     <!-- VIDEO MODULE -->
-    <div class="video_module video_module_collaborate">
+    <div class="video_module video_module_collaborate-a">
         <a href="http://www.infocus.vagrant.internal/videos?aGcZS6K92fk">
             <div class="video_link">
                 <h2 class="lead_text--secondary_headline">See how ConX Video Meeting<br>brings teams together</h2>
@@ -183,18 +183,13 @@ if($_GET['edit']=="true"){CMSscript("/resources/overviews/$pn-$lang.src", $pn, $
     <!-- END PROMO MODULES C & D -->
 
     <!-- VIDEO MODULE -->
-		<div class="hero_row hero-row--video_row hero-row--video_row_collaborate-b">
-			<div class="row">
-				<div class="small-12 medium-10 medium-offset-1 columns lead_text">
-					<h2 class="lead_text--secondary_headline">
-						<a href="http://www.infocus.vagrant.internal/videos?SNMjzPRQUA8">
-							<img src="/resources/static/images/video-icon.png" />
-							<span class="single_line_span">See how collaboration is done</span>
-						</a>
-					</h2>
-				</div>
-			</div>
-		</div>
+    <div class="video_module video_module_collaborate-b">
+        <a href="http://www.infocus.vagrant.internal/videos?SNMjzPRQUA8">
+            <div class="video_link">
+                <h2 class="lead_text--secondary_headline">See how collaboration is done</h2>
+            </div>
+        </a>
+    </div>
     <!-- END VIDEO MODULE -->
 		<script>
 		    $(document).foundation();
