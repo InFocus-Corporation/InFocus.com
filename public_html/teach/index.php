9ac84c9c592df4b2b25007c52bee53bcbb908a5e
Chris Wilson <chris@parthenonsoftware.com>
2016-05-19 11:41:29 -0700

    Collaborate copy changes, other small fixes

--- public_html/teach/index.php
+++ public_html/teach/index.php
@@ -3,7 +3,7 @@ require_once($_SERVER['DOCUMENT_ROOT']. "/resources/php/infocusscripts.php");
 require_once($_SERVER['DOCUMENT_ROOT']. "/resources/php/header.php");
 echo PHP_EOL . '<link rel="canonical" href="http://' .  $_SERVER['SERVER_NAME'] . '"/>' . PHP_EOL;
 ?>
-<title>InFocus | Present</title>
+<title>InFocus | Teach & Train</title>
 <meta name="description" content="Discover advanced projection equipment offered by InFocus. Global leader in LCD projectors and DLP projection, Mondopad products and projector accessories." />
 <meta name="keywords" content="infocus,mondopad,projector,projection,DLP,LCD,LCD display,digital display,visual communications,projector accessories,infocus company,digital technology,projection equipment,monopad, communication, touchscreen" />
 
